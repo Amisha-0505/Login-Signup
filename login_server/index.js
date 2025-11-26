b1b616791237ef73f68336f01d83ed5526d655e7
Amisha-0505 <amishashaw375@gmail.com>
2025-11-26 14:45:39 +0530

    Initial commit with server and client

---
+++ login_server/index.js
@@ -0,0 +1,25 @@
+const express=require("express");
+const mongoose=require("mongoose");
+const dotenv=require("dotenv");
+const morgan=require("morgan");
+const userRouter=require("./route/userRouter");
+const cors=require("cors");
+const app=express();
+
+dotenv.config({path:'.env'});
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.db)
+.then(()=>{
+    console.log('db connected successfully');
+})
+.catch((err)=>{
+    console.log('err'+err);
+})
+
+app.use('/',userRouter);
+
+const Port=9000;
+app.listen(Port,()=>{console.log(`Your server is running at a port ${Port}`)})
